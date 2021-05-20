0cf98919cd86758fc8f57c17a0342a66fe3bb5ff
LucasPimentaRodrigues <81471125+LucasPimentaRodrigues@users.noreply.github.com>
2021-05-20 06:44:56 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+var valorEmDolarTexto = prompt ("Qual o valor em dólar em você quer converter?")
+
+var valorEmDolarNumero = parseFloat (valorEmDolarTexto)
+
+var valorEmReal = valorEmDolarNumero * 5.50
+
+alert(valorEmReal)

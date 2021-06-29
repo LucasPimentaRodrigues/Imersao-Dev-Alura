7d5d9b5c53df57b935eaac61daf6d196194919b2
LucasPimentaRodrigues <81471125+LucasPimentaRodrigues@users.noreply.github.com>
2021-06-29 04:38:23 -0300

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-var valorEmDolarTexto = prompt ("Qual o valor em dólar em você quer converter?")
-
-var valorEmDolarNumero = parseFloat (valorEmDolarTexto)
-
-var valorEmReal = valorEmDolarNumero * 5.50
-
-alert(valorEmReal)
